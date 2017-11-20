f2b8696b07adb05674675a2f7516b853d4420366
damelLP <dlambertpowell@gmail.com>
2017-11-20 18:26:24 +0000

    Initial agent + update game class

--- mancalapy/agent.py
+++ mancalapy/agent.py
@@ -1,7 +1,63 @@
+from side import Side
+from game import Game
+
+side = Side.NORTH
+
+
 def get_msg():
-    s = input()
-    return s
+    from_engine = input().strip()
+    output_(f"from engine: {from_engine}")
+    args = from_engine.split(";")
+    return args[0], args[1:]
 
 
 def send_msg(msg):
+    output_(f"from agent: {msg}")
     print(msg)
+
+
+def output_(msg, x=False):
+    if x:
+        output_("Here")
+    with open("output.txt", "a") as f:
+        f.write(msg + "\n")
+
+
+def output_state(board):
+    north, south = board
+    output_("%s -- %s" % (north[-1], " ".join(reversed(north[:7]))))
+    output_("%s -- %s" % (" ".join(south[:7]), south[-1]))
+
+
+def make_move(board):
+    my_side = board.board[side.value]
+    output_("our side: %s" % my_side)
+    for i in range(len(my_side) - 2, -1, -1):
+        if my_side[i] > 0:
+            return i
+
+
+if __name__ == '__main__':
+    our_turn = False
+    holes = 7
+    seeds = 7
+    game = Game()
+    while True:
+        msg_type, args = get_msg()
+        if msg_type.upper() == "START":
+            if args[0].upper() == "SOUTH":
+                side = Side.SOUTH
+                our_turn = True
+            else:
+                side = Side.NORTH
+        if msg_type.upper() == "END":
+            break
+        if msg_type.upper() == "CHANGE":
+            our_turn = args[-1].upper() == "YOU"
+            game.update_board(args[-2])
+            if args[1].upper() == "SWAP":
+                side.opposite()
+            output_(game.state())
+        if our_turn:
+            move = make_move(game)
+            send_msg(f"MOVE;{move}")
--- mancalapy/game.py
+++ mancalapy/game.py
@@ -17,35 +17,38 @@ def idx_of_side(side):
         return SOUTH_ROW
 
 
-class Board:
-    def __init__(self, board=None):
-        if board:
-            self.board = board.board
-            self.holes = board.holes
-            self.seeds = board.seeds
-        else:
-            self.board = []
-            self.holes = 0
-            self.seeds = 0
+class Game:
+    def __init__(self, holes=7, seeds=7, board=None):
+        self.holes = holes
+        self.seeds = seeds
+        self.board = board if board else self.create_board(holes, seeds)
+
+    def update_board(self, raw_state):
+        state = raw_state.split(",")
+        north = [i for i in state[:8]]
+        south = [i for i in state[8:]]
+        self.board = [north, south]
 
     @classmethod
-    def from_values(cls, holes=4, seeds=7):
+    def create_board(cls, holes=7, seeds=7):
         if holes < 1:
             raise ValueError("There has to be at least one hole.")
         if seeds < 1:
             raise ValueError("There has to be a non-negative number of seeds.")
-        board = Board()
-        board.board = [
+
+        board = [
             [seeds for _ in range(holes + 1)],
             [seeds for _ in range(holes + 1)]
         ]
-        # set mancalah's to be empty
-        board.board[NORTH_ROW][-1] = 0
-        board.board[SOUTH_ROW][-1] = 0
 
-        board.holes = holes
-        board.seeds = seeds
-        return cls(board)
+        # set mancalah's to be empty
+        board[NORTH_ROW][-1] = 0
+        board[SOUTH_ROW][-1] = 0
+        return board
+
+    @classmethod
+    def from_values(cls):
+        return cls(board=cls.create_board())
 
     def check_hole(self, hole):
         if hole < 1 or hole > self.holes:
@@ -99,8 +102,12 @@ class Board:
         side_idx = idx_of_side(side)
         self.board[side_idx][0] += seeds
 
+    def state(self):
+        return self.__repr__()
+
     def __repr__(self):
         north, south = self.board
+
         north = "%d -- %s" % (north[-1], " ".join(map(str, reversed(north[:7]))))
         south = "%s -- %d" % (" ".join(map(str, south[:7])), south[-1])
         return f"{north}\n{south}"
