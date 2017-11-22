ac8de08dfa7f361ef9d74970107e801e356aba56
Harshdeep <harsh.deep@student.manchester.ac.uk>
2017-11-22 01:16:02 +0000

    Pretty much the complete structure of the Engine but integrating it with Game, will update those files later

--- mancalapy/decision_engines/decision_engine_factory.py
+++ mancalapy/decision_engines/decision_engine_factory.py
@@ -1,5 +1,8 @@
 from decision_engines import BasicStrategy
-from decision_engines.minimax_engines import MiniMaxDecisionEngine, AlphaBetaMiniMaxDecisionEngine
+from decision_engines.minimax_engines import (
+    MiniMaxDecisionEngine, AlphaBetaMiniMaxDecisionEngine
+)
+from decision_engines.monte_carlo import MonteCarloDecisionEngine
 
 
 class DecisionEngineFactory:
@@ -10,6 +13,7 @@ class DecisionEngineFactory:
             "basic": BasicStrategy(self.agent),
             "minimax": MiniMaxDecisionEngine(self.agent),
             "ab_minimax": AlphaBetaMiniMaxDecisionEngine(self.agent),
+            "monte_carlo": MonteCarloDecisionEngine(self.agent)
         }
 
     def __getitem__(self, item):
