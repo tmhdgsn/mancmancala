619baab9b023ce16d5fb5cc6e3e79a3b58f80171
damelLP <dlambertpowell@gmail.com>
2017-11-23 20:42:57 +0000

    added initial tests for minimax decision engines, more cases needed

--- mancalapy/decision_engines/tests/test_minimax.py
+++ mancalapy/decision_engines/tests/test_minimax.py
@@ -1,17 +1,31 @@
 import pytest
 
+from agent import Agent
 from game import Game
 from side import Side
 
-test_games = [
-    (Game(), 0, Side.SOUTH, (Game(board=[
-        [7, 7, 7, 7, 7, 7, 7, 0],
-        [0, 8, 8, 8, 8, 8, 8, 1]
-    ]))),
-    (Game(), 0, Side.NORTH, (Game(board=[
-        [0, 8, 8, 8, 8, 8, 8, 1],
-        [7, 7, 7, 7, 7, 7, 7, 0]
-    ])))
-]
 
-# TODO: Create test cases for minimax
+@pytest.fixture(scope="function")
+def minimax_mock_agent():
+    return Agent("minimax")
+
+
+@pytest.fixture(scope="function")
+def ab_minimax_mock_agent():
+    return Agent("minimax")
+
+
+@pytest.mark.parametrize("game, side, move", [
+    (Game(), Side.SOUTH, 1),
+])
+def test_minimax_agent_makes_correct_decision(minimax_mock_agent, game, side, move):
+    minimax_mock_agent.side = side
+    assert move == minimax_mock_agent.decision_engine.get_move(game, depth=3)
+
+
+@pytest.mark.parametrize("game, side, move", [
+    (Game(), Side.SOUTH, 1),
+])
+def test_ab_minimax_agent_makes_correct_decision(ab_minimax_mock_agent, game, side, move):
+    ab_minimax_mock_agent.side = side
+    assert move == ab_minimax_mock_agent.decision_engine.get_move(game, depth=3)
