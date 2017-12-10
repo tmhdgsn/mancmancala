9c5f788b96beaf005f3cbd7794b4eaa65e673071
damelLP <dlambertpowell@gmail.com>
2017-12-10 01:56:06 +0000

    playing with different heuristics and iterative deepening

--- mancalapy/decision_engines/tests/test_minimax.py
+++ mancalapy/decision_engines/tests/test_minimax.py
@@ -19,8 +19,8 @@ def ab_minimax_mock_agent():
 
 test_games = [
     (np.array([
-        [7, 7, 7, 7, 7, 7, 7, 0],
-        [0, 8, 8, 8, 8, 8, 8, 1]
+        [8, 7, 7, 7, 7, 7, 7, 0],
+        [7, 0, 8, 8, 8, 8, 8, 1]
     ]), Side.NORTH, False, SWAP),
     (np.array([
         [0, 0, 0, 0, 0, 2, 7, 18],
@@ -37,11 +37,11 @@ test_games = [
 def test_minimax_agent_makes_correct_decision(minimax_mock_agent, game, side, has_moved, expected_move):
     minimax_mock_agent.side = side
     minimax_mock_agent.has_moved = has_moved
-    assert expected_move == minimax_mock_agent.decision_engine.get_move(game, depth=3)
+    assert expected_move == minimax_mock_agent.decision_engine.get_move(game, start=3, thinking_time=1)
 
 
 @pytest.mark.parametrize("game, side,has_moved, expected_move", test_games)
 def test_ab_minimax_agent_makes_correct_decision(ab_minimax_mock_agent, game, side, has_moved, expected_move):
     ab_minimax_mock_agent.side = side
     ab_minimax_mock_agent.has_moved = has_moved
-    assert expected_move == ab_minimax_mock_agent.decision_engine.get_move(game, depth=3)
+    assert expected_move == ab_minimax_mock_agent.decision_engine.get_move(game, start=3, thinking_time=1)
