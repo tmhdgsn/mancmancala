0e8db03aa93408cde74cb398462401c9719acffc
damelLP <dlambertpowell@gmail.com>
2017-12-13 16:21:27 +0000

    Revert "done with setup for google test, first test for game over"
    This reverts commit 3350c96ab9b5f505aec42769dea8908e8f76c871.

--- cppmancala/main.cpp
+++ cppmancala/main.cpp
@@ -27,62 +27,53 @@ void make_move(int move) {
     }
 }
 
-//int main() {
-//    std::tuple<std::string, std::string> msg_args;
-//    std::array<int, 16> board{7, 7, 7, 7, 7, 7, 7, 0, 7, 7, 7, 7, 7, 7, 7, 0};
-//    int move;
-//    std::string last_move;
-//    char sep;
-//    int side = de::NORTH;
-//    std::string msg_type, args, player;
-//    bool our_go = false;
-//    bool has_moved = false;
-//    while (true) {
-//        msg_args = getmessage();
-//        msg_type = std::get<0>(msg_args);
-//        args = std::get<1>(msg_args);
-//        if (msg_type == "START") {
-//            if (args == "South") {
-//                side = de::SOUTH;
-//                our_go = true;
-//            }
-//        } else if (msg_type == "CHANGE") {
-//            auto pos = args.find(';');
-//            last_move = args.substr(0, pos);
-//            if (last_move == "SWAP") {
-//                our_go = true;
-//                side = de::NORTH;
-//            } else {
-//                auto remaining_args = args.substr(pos + 1);
-//                auto arg_stream = std::stringstream(remaining_args);
-//                for (auto pit = board.begin(); pit < board.end(); pit++) {
-//                    arg_stream >> *pit;
-//                    arg_stream >> sep;
-//                }
-//                arg_stream >> player;
-//                our_go = player == "YOU";
-//            }
-//        } else if (msg_type == "END") {
-//            break;
-//        }
-//        if (our_go) {
-//            move = minimax::get_move(board, side, has_moved);
-//            if (move < 0)
-//                side = 8 - side;
-//            make_move(move);
-//            if(!has_moved)
-//                has_moved = true;
-//        }
-//    }
-//    return 0;
-//}
-
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
-
-int main(int argc, char* argv[]) {
-    testing::InitGoogleTest(&argc, argv);
-    RUN_ALL_TESTS();
+int main() {
+    std::tuple<std::string, std::string> msg_args;
+    std::array<int, 16> board{7, 7, 7, 7, 7, 7, 7, 0, 7, 7, 7, 7, 7, 7, 7, 0};
+    int move;
+    std::string last_move;
+    char sep;
+    int side = de::NORTH;
+    std::string msg_type, args, player;
+    bool our_go = false;
+    bool has_moved = false;
+    while (true) {
+        msg_args = getmessage();
+        msg_type = std::get<0>(msg_args);
+        args = std::get<1>(msg_args);
+        if (msg_type == "START") {
+            if (args == "South") {
+                side = de::SOUTH;
+                our_go = true;
+            }
+        } else if (msg_type == "CHANGE") {
+            auto pos = args.find(';');
+            last_move = args.substr(0, pos);
+            if (last_move == "SWAP") {
+                our_go = true;
+                side = de::NORTH;
+            } else {
+                auto remaining_args = args.substr(pos + 1);
+                auto arg_stream = std::stringstream(remaining_args);
+                for (auto pit = board.begin(); pit < board.end(); pit++) {
+                    arg_stream >> *pit;
+                    arg_stream >> sep;
+                }
+                arg_stream >> player;
+                our_go = player == "YOU";
+            }
+        } else if (msg_type == "END") {
+            break;
+        }
+        if (our_go) {
+            move = minimax::get_move(board, side, has_moved);
+            if (move < 0)
+                side = 8 - side;
+            make_move(move);
+            if(!has_moved)
+                has_moved = true;
+        }
+    }
     return 0;
 }
 
