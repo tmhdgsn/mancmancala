9b7e62c3454cccdedc295e3a273a2232ad82ffc6
Harshdeep <harsh.deep@student.manchester.ac.uk>
2017-12-13 15:07:54 +0000

    pushing it into lib

---
+++ cppmancala/cmake-build-debug/CMakeFiles/cplusmancala.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/harshdeep/COMP34120/mancmancala/cppmancala/decision_engine.cpp" "/Users/harshdeep/COMP34120/mancmancala/cppmancala/cmake-build-debug/CMakeFiles/cplusmancala.dir/decision_engine.cpp.o"
+  "/Users/harshdeep/COMP34120/mancmancala/cppmancala/main.cpp" "/Users/harshdeep/COMP34120/mancmancala/cppmancala/cmake-build-debug/CMakeFiles/cplusmancala.dir/main.cpp.o"
+  "/Users/harshdeep/COMP34120/mancmancala/cppmancala/minimax_decision_engine.cpp" "/Users/harshdeep/COMP34120/mancmancala/cppmancala/cmake-build-debug/CMakeFiles/cplusmancala.dir/minimax_decision_engine.cpp.o"
+  "/Users/harshdeep/COMP34120/mancmancala/cppmancala/mtd_decision_engine.cpp" "/Users/harshdeep/COMP34120/mancmancala/cppmancala/cmake-build-debug/CMakeFiles/cplusmancala.dir/mtd_decision_engine.cpp.o"
+  "/Users/harshdeep/COMP34120/mancmancala/cppmancala/tests/test_de.cpp" "/Users/harshdeep/COMP34120/mancmancala/cppmancala/cmake-build-debug/CMakeFiles/cplusmancala.dir/tests/test_de.cpp.o"
+  "/Users/harshdeep/COMP34120/mancmancala/cppmancala/utils.cpp" "/Users/harshdeep/COMP34120/mancmancala/cppmancala/cmake-build-debug/CMakeFiles/cplusmancala.dir/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib/googletest/googletest/include"
+  "../lib/googletest/googlemock/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/harshdeep/COMP34120/mancmancala/cppmancala/cmake-build-debug/lib/googletest/googlemock/gtest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "/Users/harshdeep/COMP34120/mancmancala/cppmancala/cmake-build-debug/lib/googletest/googlemock/gtest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cppmancala/cmake-build-debug/CMakeFiles/cplusmancala.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cplusmancala.dir/main.cpp.o"
+  "CMakeFiles/cplusmancala.dir/utils.cpp.o"
+  "CMakeFiles/cplusmancala.dir/minimax_decision_engine.cpp.o"
+  "CMakeFiles/cplusmancala.dir/decision_engine.cpp.o"
+  "CMakeFiles/cplusmancala.dir/mtd_decision_engine.cpp.o"
+  "CMakeFiles/cplusmancala.dir/tests/test_de.cpp.o"
+  "cplusmancala.pdb"
+  "cplusmancala"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cplusmancala.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
